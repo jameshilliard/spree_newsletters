7f8e1d74d046c81b2d9db11dcb0029d82942e6cf
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-09 15:32:01 -0400

    adding icon to admin tab

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -2,6 +2,7 @@ Deface::Override.new(
     :virtual_path => "spree/layouts/admin",
     :name => "spree_newsletter_subscribers_admin_tag",
     :insert_bottom => "[data-hook='admin_tabs']",
-    :text => "<%= tab(:subscribers, :url => spree.admin_newsletter_subscribers_path) %>",
+    :text => "<%= tab(:newsletter_subscribers, :url => spree.admin_newsletter_subscribers_path, :icon => 'icon-email') %>",
+    :original => '947913b0b6e4bd6a35c042c06e6d2984a0aa094b',
     :disabled => false
 )
