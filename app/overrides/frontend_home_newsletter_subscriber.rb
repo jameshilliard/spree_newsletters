48dda29931f366d439e0ccc18fbbacda7cdada0f
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-09 17:33:12 -0400

    fixing the admin index

--- app/overrides/frontend_home_newsletter_subscriber.rb
+++ app/overrides/frontend_home_newsletter_subscriber.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
     :virtual_path => "spree/layouts/spree_application",
     :name => "spree_newsletter_subscribers_home_input",
-    :insert_after => "div#wrapper",
-    :text => "<div><%= form_for :newsletter_subscriber, :url => :newsletter_subscribers do |f| %><%= f.text_field :email %><%= f.submit 'SUBSCRIBE' %><% end %></div>",
+    :insert_before => "div#footer",
+    :text => "<div><%= form_for :newsletter_subscriber, :url => :newsletter_subscribers do |f| %><%= f.text_field :email %><%= f.submit Spree.t(:subscribe) %><% end %></div>",
     :disabled => false
 )
