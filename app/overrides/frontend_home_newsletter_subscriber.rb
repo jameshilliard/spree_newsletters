da6fa74d7635e3bc3e4cf4defd9f83d4f0888710
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-09 20:25:41 -0400

    moving the main form

--- app/overrides/frontend_home_newsletter_subscriber.rb
+++ app/overrides/frontend_home_newsletter_subscriber.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
     :virtual_path => "spree/shared/_footer",
     :name => "spree_newsletter_subscribers_home_input",
-    :insert_before => "footer#footer",
+    :insert_bottom => "id#wrapper",
     :text => "<div><%= form_for :newsletter_subscriber, :url => :newsletter_subscribers do |f| %><%= f.text_field :email %><%= f.submit Spree.t(:subscribe) %><% end %></div>",
     :disabled => false
 )
