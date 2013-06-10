f6274ff949967144a57ea7cfc8a4cd1218bce8d8
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-09 20:39:05 -0400

    removing temp the admin new button

--- app/overrides/frontend_home_newsletter_subscriber.rb
+++ app/overrides/frontend_home_newsletter_subscriber.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-    :virtual_path => "spree/shared/_footer",
+    :virtual_path => "spree/layouts/spree_application",
     :name => "spree_newsletter_subscribers_home_input",
     :insert_bottom => "id#wrapper",
     :text => "<div><%= form_for :newsletter_subscriber, :url => :newsletter_subscribers do |f| %><%= f.text_field :email %><%= f.submit Spree.t(:subscribe) %><% end %></div>",
