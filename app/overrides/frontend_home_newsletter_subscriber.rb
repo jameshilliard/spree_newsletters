98c1a97605353bfc3f6c8a52387fd6c864ee576c
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-18 11:17:46 -0300

    merge

--- app/overrides/frontend_home_newsletter_subscriber.rb
+++ app/overrides/frontend_home_newsletter_subscriber.rb
@@ -1,7 +1,8 @@
 Deface::Override.new(
     :virtual_path => "spree/layouts/spree_application",
     :name => "spree_newsletter_subscribers_home_input",
-    :insert_bottom => "id#wrapper",
+    :insert_bottom => "div#wrapper",
+		:original => 'b783e7268d2ab196e65a32bc048cf6af2627dd06',
     :text => "<div><%= form_for :newsletter_subscriber, :url => :newsletter_subscribers do |f| %><%= f.text_field :email %><%= f.submit Spree.t(:subscribe) %><% end %></div>",
     :disabled => false
 )
