84f84c0a0fc17393349dd6854e3a72353c454423
James Hilliard <james.hilliard1@gmail.com>
2014-07-17 14:42:46 -0600

    fix deface paths

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => "spree/admin/shared/_menu",
   :name => "spree_newsletter_subscribers_admin_tag",
-  :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+  :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
   :text => "<%= tab(Spree.t(:newsletter_subscribers), :url => spree.admin_newsletter_subscribers_path, :icon => 'icon-email') %>",
   :disabled => false
 )
