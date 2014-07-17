97a04158e1ebca6d6f301d21f298e64d87c248ab
James Hilliard <james.hilliard1@gmail.com>
2014-07-17 14:48:42 -0600

    update admin override

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   :virtual_path => "spree/admin/shared/_menu",
   :name => "spree_newsletter_subscribers_admin_tag",
   :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-  :text => "<%= tab(Spree.t(:newsletter_subscribers), :url => spree.admin_newsletter_subscribers_path, :icon => 'icon-email') %>",
+  :text => "<%= configurations_sidebar_menu_item Spree.t(:newsletter_subscribers), admin_newsletter_subscribers_path %>",
   :disabled => false
 )
